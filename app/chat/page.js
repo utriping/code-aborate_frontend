c9672a6e9392c4b9b8873560db48e40666016778
utriping <utripathiwork@gmail.com>
2026-02-22 15:08:31 +0530

    feat: enhance chat page with message input and user profile updates; improve styling and scrollbar handling

--- app/chat/page.js
+++ app/chat/page.js
@@ -1,7 +1,9 @@
-import { Send, Settings } from "lucide-react";
-import React from "react";
+"use client";
+import { MoreVertical, Send, Settings, Sword } from "lucide-react";
+import React, { useState } from "react";
 
 const page = () => {
+  const [newMessage, setNewMessage] = useState("");
   //click a chat to see the messsages --> e= clicked Raj -->
   const messages = [
     { message: "Hey, how are you doing?", sentBy: "Raj" },
@@ -35,9 +37,9 @@ const page = () => {
     { name: "Amit", points: 60, message: "Sure, let me take a look at it." },
   ];
   return (
-    <section className="my-10 border border-[#B10F2E] h-screen">
-      <div className="grid grid-cols-1 lg:grid-cols-[30%_1fr] h-screen overflow-y-scroll ">
-        <div className="w-full bg-[#B10F2E]">
+    <section className="my-10 border border-[#B10F2E] h-screen p-3">
+      <div className="grid grid-cols-1 lg:grid-cols-[30%_1fr] h-screen rounded-lg">
+        <div className="w-full bg-[#B10F2E] overflow-y-scroll scrollbar-none">
           <div className="h-22 bg-[#B10F2E] text-2xl font-bold p-3 flex flex-cols gap-4 items-center border-b-2 border-[#f3dfc1] border-r-none sticky top-0">
             <div className="rounded-full w-14 h-14 bg-white inline-flex items-center justify-center text-2xl font-bold">
               YOU
@@ -60,13 +62,29 @@ const page = () => {
           })}
           <div className="">Add more friends to chat with more people</div>
         </div>
-        <div className="hidden lg:block lg:w-full bg-[#B10F2E] border-l-2 border-[#f3dfc1]">
+
+        <div className="hidden lg:block lg:w-full bg-[#B10F2E] border-l-2 border-[#f3dfc1] h-screen overflow-y-scroll scrollbar-none relative">
           <div className="h-22 text-2xl font-bold p-3 flex flex-cols gap-4 items-center border-b-2 border-[#f3dfc1] border-r-none sticky top-0">
             <div className="rounded-full w-14 h-14 bg-white inline-flex items-center justify-center text-2xl font-bold">
-              YOU
+              R
             </div>
-            <div className="text-[#F3DFC1]">Your Profile</div>
-            <Settings className="ml-auto text-[#F3DFC1]" size={32}></Settings>
+            <div className="text-[#F3DFC1]">Rohit</div>
+            <MoreVertical
+              className="ml-auto text-[#F3DFC1] cursor-pointer"
+              size={32}
+            ></MoreVertical>
+          </div>
+
+          {/* chat */}
+          {/* send message bar */}
+          <div className="absolute bottom-0 bg-[#8E0C25] w-full flex items-center p-3 text-[#F3DFC1] ">
+            <input
+              type="text"
+              className=""
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <Sword className="ml-auto text-[#F3DFC1]" size={32} />
           </div>
         </div>
       </div>
